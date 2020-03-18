7ce09634edce57ae3d703a3020f829217e96b607
kagafon <kagafon@users.noreply.github.com>
2020-03-18 21:30:27 +0300

    fix: restore navigation item activation on scroll fix: rework gallery items shuffling (remove-and-add instead of flex-order change) fix: add form reset on modal window close

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 const slides = document.querySelectorAll(".slide");
 const slidesContainer = document.querySelector(".slider__content");
-const galleryImages = [...Array(12)].map((el, idx) => {
-    return { "src": `assets/portfolio-${idx + 1}.png`, "alt": `Portfolio ${idx + 1}`};
-});
-const galleryPlaceholders = [...document.querySelectorAll(".gallery__item")];
+const galleryItems = [...document.querySelectorAll(".gallery__item")];
+const galleryContainer = document.querySelector(".gallery__content");
+const sectionPositions = [...document.querySelectorAll(".header__navigation__item > a")].reduce((res, navItem) => {
+    let targetElement = document.getElementById(navItem.getAttribute("href").substring(1));
+    if (targetElement){
+        res[navItem.getAttribute("href")] = {
+            'top': targetElement.offsetTop, 
+            'height': targetElement.offsetHeight,
+            'bottom': targetElement.offsetTop + targetElement.offsetHeight,
+            'middle': targetElement.offsetTop + targetElement.offsetHeight/2,
+            'navItem': navItem.parentElement
+        }
+    }
+    return res;}, {});
+const headerSize = 95;
 let currentGalleryOrder = [...Array(12)].map((el, idx) => idx);
 let activeSlideIdx = 0;
 let disableSlideControls = false;
+let prevYOffset = 0;
 
 /* Helpers */
 const setOnlyOneActive = (element, selectClassName, container, activeClassName) => {
@@ -14,7 +26,7 @@ const setOnlyOneActive = (element, selectClassName, container, activeClassName) 
     if (container === undefined) container = document;
     container.querySelectorAll("."+selectClassName).forEach(el => el.classList.remove(activeClassName));
     element.classList.add(activeClassName);
-}
+};
 
 /* Navigation */
 document.querySelector(".header__navigation > ul").addEventListener("click", (event) => {
@@ -23,13 +35,63 @@ document.querySelector(".header__navigation > ul").addEventListener("click", (ev
         if (href.startsWith("#") && href.length > 1){
             const targetElement = document.getElementById(href.substring(1));
             if (targetElement){
-                window.scrollTo(0, targetElement.offsetTop - document.querySelector("header").clientHeight);
-                event.preventDefault();
-                setOnlyOneActive(event.target.parentElement, "header__navigation__item");
+                /* Some magic: if section size less than screen size (part of it) 
+                    then make section in the middle of the screen */
+                if (sectionPositions[href].height < innerHeight/2-headerSize){
+                    window.scrollTo(0, sectionPositions[href].middle - innerHeight / 2);
+                    event.preventDefault();
+                }
             }
         }
     }
-})
+});
+
+window.onscroll = () => {
+    let sectionsArray = [];
+    if (prevYOffset > pageYOffset){
+        //Scrolling up
+        Object.values(sectionPositions).some((x, idx) => {
+            /* If top of section is visible - set it active */
+            if (x.top >= pageYOffset + headerSize && x.top < pageYOffset + innerHeight){
+                setOnlyOneActive(x.navItem, "header__navigation__item");
+                return true;
+            }
+        });
+    }
+    else
+    {
+        //Scrolling down
+        /* Go from the last section */
+        Object.values(sectionPositions).reverse().some((x, idx) => {
+            if (idx == 0){
+                /* if last section is all on screen - make it active */
+                if (x.bottom <= pageYOffset + innerHeight){
+                    setOnlyOneActive(x.navItem, "header__navigation__item");
+                    return true;
+                }
+            }
+            if (x.height > innerHeight){
+                /* if section height greater then screen size
+                    and sections top is in the higher part of screen - make it active */
+                if (x.top <= pageYOffset + innerHeight / 2 && x.top > pageYOffset + headerSize){
+                    setOnlyOneActive(x.navItem, "header__navigation__item");
+                    return true;
+                }                                
+            }
+            else{
+                /* if section height less then screen size
+                    and sections middle is in the higher part of screen - make it active */
+                    if (x.middle <= pageYOffset + innerHeight * 2 / 3 && x.middle > pageYOffset){
+                    setOnlyOneActive(x.navItem, "header__navigation__item");
+                    return true;
+                }
+            }
+            return false;
+        });
+    }
+    prevYOffset = pageYOffset;
+};
+window.onscroll();
 
 /* Screen blackout */
 document.querySelectorAll(".iphone__button").forEach(btn => btn.addEventListener("click", (event) => {
@@ -67,42 +129,39 @@ const createSlideHandler = (direction) => {
         };
         animation.play();
     };
-}
+};
 
 document.querySelector(".slider__control_left").addEventListener("click", createSlideHandler(-1));
 document.querySelector(".slider__control_right").addEventListener("click", createSlideHandler(1));
 
 /* Gallery */
-const reloadGallery = (filter) => {
-    if (filter){
-        /* Ensure shuffling */
-        const newGalleryOrder = [...currentGalleryOrder];
-        while(newGalleryOrder.some((val, idx) => currentGalleryOrder[idx] == val)){
-            newGalleryOrder.sort((a, b) => Math.random()-0.5);
-        }
-
-        galleryPlaceholders.forEach((placeholder, idx) => {
-            placeholder.classList.remove("gallery__item_active")
-            placeholder.style.order = newGalleryOrder[idx];
-            placeholder.animate([
-                {opacity:0},
-                {src: galleryImages[currentGalleryOrder[idx]].src},
-                {src: galleryImages[newGalleryOrder[idx]].src},
-                {opacity: 1}
-            ], {
-                duration: 500,
-                easing: "ease-out",
-            });
-            });
-        currentGalleryOrder = newGalleryOrder;
-    }
-}
-
 document.querySelector(".gallery__filter").addEventListener("click", (event) => {
-    if (event.target.classList.contains("gallery__filter__button") 
+    if (event.target.classList.contains("gallery__filter__button")
         && !event.target.classList.contains("gallery__filter__button_active")){
         setOnlyOneActive(event.target, "gallery__filter__button", event.currentTarget);
-        reloadGallery(event.target.dataset.filter);
+        if (event.target.dataset.filter){
+            /* Ensure shuffling */
+            const newGalleryOrder = [...currentGalleryOrder];
+            while(newGalleryOrder.some((val, idx) => currentGalleryOrder[idx] == val)){
+                newGalleryOrder.sort((a, b) => Math.random()-0.5);
+            }
+            galleryContainer.animate(
+                [{opacity:1}, {opacity: 0}], 
+                {
+                    duration: 500,
+                    easing: "ease-out",
+            });
+
+            galleryItems.forEach(x => {x.classList.remove("gallery__item_active");x.remove();});
+            newGalleryOrder.forEach(x => galleryContainer.appendChild(galleryItems[x]));
+            galleryContainer.animate(
+                [{opacity:0}, {opacity: 1}], 
+                {
+                    duration: 500,
+                    easing: "ease-out",
+            });            
+            currentGalleryOrder = newGalleryOrder;
+        }
     }
 });
 
@@ -130,4 +189,5 @@ document.querySelector(".get-a-quote-form").addEventListener("submit", (event) =
 
 document.querySelector(".modal-window__field_close").addEventListener('click', (event) =>{
     document.querySelector(".modal-window").classList.remove("modal-window_shown");
+    document.querySelector(".get-a-quote-form").reset();
 });
